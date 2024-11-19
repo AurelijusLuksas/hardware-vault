bd536727785a4bf2959023d3225fde8b0bc7d173
Aurelijus Lukšas <aurelijus.luksas@mif.stud.vu.lt>
2024-11-19 20:57:31 +0200

    initial commit

---
+++ hardware-vault/src/test/java/main/hardwarevault/HardwareVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package main.hardwarevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
