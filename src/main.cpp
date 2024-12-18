20283e59463a0264a04205f0aa6c4e706bf897da
Aurelijus Lukšas <aurelijus.luksas@mif.stud.vu.lt>
2024-12-18 16:57:15 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ char* getpass(const char* prompt) {
 EXEC SQL BEGIN DECLARE SECTION;
 int result;
 char password[MAX_PASSWORD_LENGTH];
-char username[9] = "username";
+char username[9] = "name";
 EXEC SQL END DECLARE SECTION;
 
 int main() {
@@ -43,7 +43,7 @@ int main() {
 
     printf("CONNECT TO biblio. SQLCODE=%ld\n", SQLCODE);
     if (0 == SQLCODE) {
-        EXEC SQL SELECT COUNT(*) INTO :result FROM username.product;
+        EXEC SQL SELECT COUNT(*) INTO :result FROM product;
     }
     if (0 == SQLCODE) {
         printf("Programa veikia! Produktu skaicius=%d\n", result);
@@ -66,13 +66,168 @@ int main() {
         switch (choice) {
             case 1:
                 std::cout << "Duomenu paieska\n";
+                while (true) {
+                    std::cout << "-------------------\n";
+                    std::cout << "[1] Rasti darbuotoja\n";
+                    std::cout << "[2] Rasti produkta\n";
+                    std::cout << "[3] Rasti uzsakyma\n";
+                    std::cout << "[4] Rasti produkto specifikacija\n";
+                    std::cout << "[5] Rasti kategorijos produktus\n";
+                    std::cout << "[6] Rasti produkto tiekejus\n";
+                    std::cout << "[7] Rasti produkto kieki sandelyje";
+                    std::cout << "[8] Rasti kliento uzsakymus\n";
+                    std::cout << "[0] Grizti\n";
+
+                    int choiceFind;
+                    std::cin >> choiceFind;
+
+                    switch (choiceFind) {
+                        case 1:
+                            std::cout << "Prideti nauja produkta\n";
+                            break;
+                        case 2:
+                            std::cout << "Prideti nauja kategorija\n";
+                            break;
+                        case 3:
+                            std::cout << "Prideti nauja klienta\n";
+                            break;
+                        case 4:
+                            std::cout << "Prideti nauja uzsakyma\n";
+                            break;
+                        case 5:
+                            std::cout << "Prideti nauja sandeli\n";
+                            break;
+                        case 6:
+                            std::cout << "Prideti nauja darbuotoja\n";
+                            break;
+                        case 7:
+                            std::cout << "Prideti nauja tiekeja\n";
+                            break;
+                        case 8:
+                            std::cout << "Prideti nauja specifikacija\n";
+                            break;
+                        case 0:
+                            std::cout << "Grizti\n";
+                            break;
+                        default:
+                            std::cout << "Neteisingas pasirinkimas\n";
+                            break;
+                    }
+                    if (choiceFind == 0) {
+                        break;
+                    }
+                }
+
                 break;
             case 2:
                 std::cout << "Duomenu ivedimas\n";
-                std::cout << "-------------------\n";
+                while (true) {
+                    std::cout << "-------------------\n";
+                    std::cout << "[1] Prideti nauja produkta\n";
+                    std::cout << "[2] Prideti nauja kategorija\n";
+                    std::cout << "[3] Prideti nauja klienta\n";
+                    std::cout << "[4] Prideti nauja uzsakyma\n";
+                    std::cout << "[5] Prideti nauja sandeli\n";
+                    std::cout << "[6] Prideti nauja darbuotoja\n";
+                    std::cout << "[7] Prideti nauja tiekeja\n";
+                    std::cout << "[8] Prideti nauja specifikacija\n";
+                    std::cout << "[0] Grizti\n";
+
+                    int choiceAdd;
+                    std::cin >> choiceAdd;
+
+                    switch (choiceAdd) {
+                        case 1:
+                            std::cout << "Prideti nauja produkta\n";
+                            break;
+                        case 2:
+                            std::cout << "Prideti nauja kategorija\n";
+                            break;
+                        case 3:
+                            std::cout << "Prideti nauja klienta\n";
+                            break;
+                        case 4:
+                            std::cout << "Prideti nauja uzsakyma\n";
+                            break;
+                        case 5:
+                            std::cout << "Prideti nauja sandeli\n";
+                            break;
+                        case 6:
+                            std::cout << "Prideti nauja darbuotoja\n";
+                            break;
+                        case 7:
+                            std::cout << "Prideti nauja tiekeja\n";
+                            break;
+                        case 8:
+                            std::cout << "Prideti nauja specifikacija\n";
+                            break;
+                        case 0:
+                            std::cout << "Grizti\n";
+                            break;
+                        default:
+                            std::cout << "Neteisingas pasirinkimas\n";
+                            break;
+                    }
+                    if (choiceAdd == 0) {
+                        break;
+                    }
+                }
                 break;
             case 3:
                 std::cout << "Duomenu atnaujinimas\n";
+                
+                while (true) {
+                    std::cout << "-------------------\n";
+                    std::cout << "[1] Pakeisti produkto kaina\n";
+                    std::cout << "[2] Pakeisti produkto kieki\n";
+                    std::cout << "[3] Pakeisti pakeisti produkto kieki uzsakyme\n";
+                    std::cout << "[4] Prideti produkta prie uzsakymo\n";
+                    std::cout << "[5] Ismesti produkta is darbo\n";
+                    std::cout << "[6] Pakeisti vartotojo varda\n";
+                    std::cout << "[7] Pakeisti produkto specifikacija\n";
+                    std::cout << "[8] Pakeisti tiekejo pristatymo kaina\n";
+                    std::cout << "[0] Grizti\n";
+
+                    int choiceUpdate;
+                    std::cin >> choiceUpdate;
+
+                    switch (choiceUpdate) {
+                        case 1:
+                            std::cout << "Prideti nauja produkta\n";
+                            break;
+                        case 2:
+                            std::cout << "Prideti nauja kategorija\n";
+                            break;
+                        case 3:
+                            std::cout << "Prideti nauja klienta\n";
+                            break;
+                        case 4:
+                            std::cout << "Prideti nauja uzsakyma\n";
+                            break;
+                        case 5:
+                            std::cout << "Prideti nauja sandeli\n";
+                            break;
+                        case 6:
+                            std::cout << "Prideti nauja darbuotoja\n";
+                            break;
+                        case 7:
+                            std::cout << "Prideti nauja tiekeja\n";
+                            break;
+                        case 8:
+                            std::cout << "Prideti nauja specifikacija\n";
+                            break;
+                        case 0:
+                            std::cout << "Grizti\n";
+                            break;
+                        default:
+                            std::cout << "Neteisingas pasirinkimas\n";
+                            break;
+                    }
+                    if (choiceUpdate == 0) {
+                        break;
+                    }
+                }
+
                 break;
             case 4:
                 std::cout << "Duomenu trynimas\n";
